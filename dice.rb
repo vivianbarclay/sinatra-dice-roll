13ff0199b79b855a246b185049a3648cea32253b
vivianbarclay <vivian.m.barclay@gmail.com>
2025-01-28 02:11:55 +0000

    Checkpoint 3

--- dice.rb
+++ dice.rb
@@ -10,17 +10,21 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
+# get("/") do
+#    <<~HTML
+#     <h1>Dice Roll</h1>
+#     <ul>
+#       <li><a href="/dice/2/6">Roll two 6-sided dice</a></li>
+#       <li><a href="/dice/2/10">Roll two 10-sided dice</a></li>
+#       <li><a href="/dice/1/20">Roll one 20-sided die</a></li>
+#       <li><a href="/dice/5/4">Roll five 4-sided dice</a></li>
+#     </ul>
+#   HTML
+# end  
+
 get("/") do
-  <<~HTML
-    <h1>Dice Roll</h1>
-    <ul>
-      <li><a href="/dice/2/6">Roll two 6-sided dice</a></li>
-      <li><a href="/dice/2/10">Roll two 10-sided dice</a></li>
-      <li><a href="/dice/1/20">Roll one 20-sided die</a></li>
-      <li><a href="/dice/5/4">Roll five 4-sided dice</a></li>
-    </ul>
-  HTML
-end  
+  erb(:elephant)
+end
 
 #simulates 2 6-sided dice
 get("/dice/2/6") do
